6224c5cb72c6a230cec5c8ba29c14bad7c4feeae
munhyeon <wmh0305@naver.com>
2024-04-11 11:08:36 +0000

    init:main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from typing import List
+def twoSum(nums: List[int], target: int) -> List[int]:
+    
+    for i in range(0,len(nums)) :   
+        output = list()
+        output.append(i)
+        for j in range(i+1,len(nums)) : 
+            if nums[i] + nums[j] == target : 
+                output.append(j)
+                return output
+        
+
